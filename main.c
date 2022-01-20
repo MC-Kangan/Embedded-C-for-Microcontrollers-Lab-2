5e79bd21742bd259742bc0bc6e988bab230002f6
MC-Kangan <97443307+MC-Kangan@users.noreply.github.com>
2022-01-20 11:17:00 +0000

    Exercise 1 Task 2

--- main.c
+++ main.c
@@ -11,11 +11,14 @@ void main(void)
 {
 	unsigned int count=0;
     LEDarray_init();
-  
+    int arr[] = { 0,1,2,4,8,16,32,64,128,64,32,16,8,4,2,1 };//create an array that contains the number that could lit up the LED
     while (1) {
-		count++; // increment count
-		if (count>511) {count=0;} //reset a when it gets too big
-		LEDarray_disp_bin(count); //output a on the LED array in binary
+		count++; // increment count (
+        //Calculate length of the array (Number of elements): length of array =  size of array / size of any one element
+        int Length  = sizeof(arr)/sizeof(arr[0]);
+		if (count >Length-1) {count=0;} //reset when count > the length of the array; Length - 1 cuz it counts from 0
+        LEDarray_disp_bin(arr[count]); //output a on the LED array in binary
+        
 		__delay_ms(50); // Delay so human eye can see change
     }
 }
